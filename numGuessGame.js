51f02ed989d193580594197aeb1419e5e8c80759
MersenneInteger <zachjohnsonsoftware@gmail.com>
2019-05-31 16:57:02 -0500

    use event listener instead of function call

--- numGuessGame.js
+++ numGuessGame.js
@@ -1,18 +1,18 @@
-function guessNum() {
+document.querySelector('.guessNumBtn').addEventListener('click', function () {
 
-    let number = null, guessNum;
+  let number = null, guessNum;
+
+  number = Math.floor((Math.random() * 10) + 1);
+  console.log('random number: ' + number);
   
-    number = Math.floor((Math.random() * 10) + 1);
-    console.log('random number' + number);
-    while(number != guessNum) {
-      guessNum = prompt('Guess a number between 1 and 10');
-      if(guessNum == null)
-        return;
-      if(parseInt(guessNum) == NaN)
-        alert('Not a number, guess again');
-      else if(number != guessNum)
-        alert('Incorrect, guess again');
-    }
-    if(number == guessNum)
-      alert('Correct!');
+  while(number != guessNum) {
+
+    guessNum = prompt('Guess a number between 1 and 10');
+    if(guessNum == number || guessNum == null)
+      break;
+    else if(number != guessNum)
+      alert('Incorrect, guess again');
   }
+  if(number == guessNum)
+    alert('Correct!');
+});
